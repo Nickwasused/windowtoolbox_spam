09843786af259bce0a0c70dfee1bb78a280be47b
Nickwasused <34883496+Nickwasused@users.noreply.github.com>
2022-04-09 16:32:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,37 +18,31 @@ head = {
 }
 
 while True:
-    test = requests.get(getenv('target'), verify=False).text
-    if (test != "null"):
-        fake_data = requests.get(fake_data_url, headers=head, verify=False).text
-        fake_data = json.loads(fake_data)
+    fake_data = requests.get(fake_data_url, headers=head, verify=False).text
+    fake_data = json.loads(fake_data)
 
-        thread_limit = round(len(fake_data)/thread_count)
+    thread_limit = round(len(fake_data)/thread_count)
 
-        limits = []
-        while True:
-            if (len(limits) == 0):
-                limits.append(thread_limit)
-            else:
-                limits.append(thread_limit + limits[len(limits)-1])
-                if (limits[len(limits)-1] > len(fake_data)):
-                    limits.pop(len(limits)-1) 
-                    limits.append(len(fake_data))
-                    break
+    limits = []
+    while True:
+        if (len(limits) == 0):
+            limits.append(thread_limit)
+        else:
+            limits.append(thread_limit + limits[len(limits)-1])
+            if (limits[len(limits)-1] > len(fake_data)):
+                limits.pop(len(limits)-1) 
+                limits.append(len(fake_data))
+                break
 
-        threads = []
+    threads = []
 
-        x = 0
-        y = 0
-        for limit in limits:
-            threads.append(Sender(fake_data[x:limit], y))
-            print("{}:{}".format(x, limit))
-            x = limit
-            y = y + 1
+    x = 0
+    y = 0
+    for limit in limits:
+        threads.append(Sender(fake_data[x:limit], y))
+        print("{}:{}".format(x, limit))
+        x = limit
+        y = y + 1
 
-        for thread in threads:
-            thread.start()
-
-        time.sleep(60)
-
-    time.sleep(10)
+    for thread in threads:
+        thread.start()
